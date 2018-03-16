cbe689cd55d3d5ff0ac5ccee76b476a1b34f3e52
kennyhong123 <31251440+kennyhong123@users.noreply.github.com>
2018-03-16 09:26:46 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -13,6 +13,13 @@ app.debug = True #Change this to False for production
 app.secret_key = os.environ['SECRET_KEY'] #used to sign session cookies
 oauth = OAuth(app)
 
+url = 'mongodb://{}:{}@{}:{}/{}'.format(
+        os.environ["MONGO_USERNAME"],
+        os.environ["MONGO_PASSWORD"],
+        os.environ["MONGO_HOST"],
+        os.environ["MONGO_PORT"],
+        os.environ["MONGO_DBNAME"])
+
 #Set up GitHub as OAuth provider
 github = oauth.remote_app(
     'github',
